3d8b9444592a5a5578df22317986699df6059b77
Ankit Mahato <ankitmahato1212@gmail.com>
2025-07-05 14:57:24 +0530

    sql

--- advansts/dist/index.js
+++ advansts/dist/index.js
@@ -1,15 +1,27 @@
 "use strict";
-//readonly
-//records
-//map
-const user = new Map();
-user.set("rs500", { name: "Ankit", age: 24, email: "email@gmail.com", pass: "pass" });
-user.set("rs501", { name: "ram", age: 24, email: "email1@gmail.com", pass: "pass" });
-user.set("rs502", { name: "Ankita", age: 24, email: "email3@gmail.com", pass: "pass" });
-user.set("rs503", { name: "rokcy", age: 24, email: "email2@gmail.com", pass: "pass" });
-user.set("rs504", { name: "tufan", age: 24, email: "email4@gmail.com", pass: "pass" });
-const users = user.get("rs504");
-const handleEvent = (event) => {
-    console.log(`Handling Event:${event}`);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-handleEvent('Click');
+Object.defineProperty(exports, "__esModule", { value: true });
+const zod_1 = require("zod");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// Define the schema for profile update
+const userProfileSchema = zod_1.z.object({
+    name: zod_1.z.string().min(1, { message: "Name cannot be empty" }),
+    email: zod_1.z.string().email({ message: "Invalid email format" }),
+    age: zod_1.z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+});
+app.put("/user", (req, res) => {
+    const { success } = userProfileSchema.safeParse(req.body);
+    const updateBody = req.body; // how to assign a type to updateBody?
+    if (!success) {
+        res.status(411).json({});
+        return;
+    }
+    // update database here
+    res.json({
+        message: "User updated"
+    });
+});
+app.listen(3000);
---
+++ advansts/dist/index1.js
@@ -0,0 +1,15 @@
+"use strict";
+//readonly
+//records
+//map
+const user = new Map();
+user.set("rs500", { name: "Ankit", age: 24, email: "email@gmail.com", pass: "pass" });
+user.set("rs501", { name: "ram", age: 24, email: "email1@gmail.com", pass: "pass" });
+user.set("rs502", { name: "Ankita", age: 24, email: "email3@gmail.com", pass: "pass" });
+user.set("rs503", { name: "rokcy", age: 24, email: "email2@gmail.com", pass: "pass" });
+user.set("rs504", { name: "tufan", age: 24, email: "email4@gmail.com", pass: "pass" });
+const users = user.get("rs504");
+const handleEvent = (event) => {
+    console.log(`Handling Event:${event}`);
+};
+handleEvent('Click');
